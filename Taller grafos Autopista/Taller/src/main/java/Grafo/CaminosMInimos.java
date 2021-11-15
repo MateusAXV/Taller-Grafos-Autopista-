2bd6276a44e82deb638f0d3660224924959c4926
santy <santiagoramirezm@ucundinamarca.edu.co>
2021-11-15 10:29:27 -0500

    videoterminado

--- Taller grafos Autopista/Taller/src/main/java/Grafo/CaminosMInimos.java
+++ Taller grafos Autopista/Taller/src/main/java/Grafo/CaminosMInimos.java
@@ -11,78 +11,148 @@ package Grafo;
  */
 public class CaminosMInimos {
     //metodo para determinar todos los caminos 
+    public String algoritmoFloid( long [][] mAdy){
 
-    public String algoritmoFloid(long[][] mAdy) {
+    
 
         int vertices = mAdy.length;
-        long matrizAdyacencia[][] = mAdy;
-        String caminos[][] = new String[vertices][vertices];
-        String caminosAuxiliares[][] = new String[vertices][vertices];
-        String caminosRecorridos = "", cadena = "", caminitos = "";
-        int i, j, k;
+
+        long matrizAdyacencia [][] = mAdy;
+
+        String caminos [][] = new String [vertices][vertices];
+
+        String caminosAuxiliares [][]=new String [vertices][vertices];
+
+        String caminoRecorrido ="",cadena="",caminitos="";
+
+        int i,j,k;
+
         float temporal1, temporal2, temporal3, temporal4, minimo;
 
-        //inicializar la matrices caminos y caminos auxiliares
-        for (i = 0; i < vertices; i++) {
-            for (j = 0; j < vertices; j++) {
-                caminos[i][j] = "";
-                caminosAuxiliares[i][j] = "";
+        //inicializar matrices y caminos 
+
+        for(i=0;i<vertices;i++) {
+
+            for(j=0;j<vertices;j++){
+
+                caminos[i][j]="";
+
+                caminosAuxiliares[i][j]="";
+
             }
+
         }
 
-        for (k = 0; k < vertices; k++) {
-            for (i = 0; i < vertices; i++) {
-                for (j = 0; j < vertices; j++) {
-                    temporal1 = matrizAdyacencia[i][j];
-                    temporal2 = matrizAdyacencia[i][k];
-                    temporal3 = matrizAdyacencia[k][j];
-                    temporal4 = temporal2 = temporal3;
-                    //encontrar minimo     
-                    minimo = Math.min(temporal1, temporal4);
-                    if (temporal1 != temporal4) {
-                        if (minimo == temporal4) {
-                            caminosRecorridos = "";
-                            caminosAuxiliares[i][j] = k + "";
-                            caminos[i][j] = caminosR(i, k, caminosAuxiliares, caminosRecorridos) + (k + 1);
-                        }
+        
+
+        for(k=0;k<vertices; k++){
+
+            for(i=0;i<vertices; i++){
+
+               for (j=0;j<vertices; j++){
+
+                   
+
+            temporal1=matrizAdyacencia [i][j];
+
+            temporal2=matrizAdyacencia [i][k];
+
+            temporal3=matrizAdyacencia [k][j];
+
+            temporal4= temporal2 + temporal3;
+
+            //encontrando al minimo
+
+            minimo=Math.min(temporal1, temporal4);
+
+            if(temporal1!=temporal4){
+
+                if(minimo == temporal4){
+
+                    caminoRecorrido="";
+
+                    caminosAuxiliares[1][j] = k + "";
+
+                    caminos[i][j]= caminosR(i,k,caminosAuxiliares, caminoRecorrido) + (k+1);
+
+                          
+
+                }
+
+            }
+
+            matrizAdyacencia[i][j]=(long) minimo;
+
+               }
+
+            }
+
+        }
+
+    //agregando el camino a cadeba
+
+    for(i=0;i<vertices;i++) {
+
+            for(j=0;j<vertices;j++){
+
+                cadena=cadena+"["+matrizAdyacencia[i][j]+"]";
+
+            }
+
+            cadena=cadena+"\n";
+
+        }
+
+    for(i=0;i<vertices;i++) {
+
+            for(j=0;j<vertices;j++){
+
+                if(matrizAdyacencia[i][j]!=1000000000){
+
+                    if(i!=j){
+
+                        if(caminos[i][j].equals("")){
+
+                            caminitos += "de ("+ (i+1)+"---->"+(j+1)+") irse por...("+(i+1)+", "+(j+1)+")\n";
+
+                        }else{ caminitos +="de ("+ (i+1)+"---->"+(j+1)+") irse por...("+(i+1)+", "+caminos[i][j] + ", " +(j+1)+")\n";
+
                     }
 
-                    matrizAdyacencia[i][j] = (long) minimo;
                 }
-            }
-        }
-
-        //agregando el camino a cadena
-        for (i = 0; i < vertices; i++) {
-            for (j = 0; j < vertices; j++) {
-                cadena = cadena + "[" + matrizAdyacencia[i][j] + "]";
 
             }
-            cadena = cadena + "\n";
+
         }
 
-        ///////////////////////////////////////
-        for (i = 0; i < vertices; i++) {
-            for (j = 0; j < vertices; j++) {
-                if (matrizAdyacencia[i][j] != 1000000000) {
-                    if (i != j) {
-                        if (caminos[i][j].equals("")) {
-                            caminitos += "De (" + (i + 1) + "--->" + (j + 1)
-                                    + ") irse por...(" + (i + 1) + ", "
-                                    + (j + 1) + ")\n";
-                        } else {
-                            caminitos += "De (" + (i + 1) + "--->" + (j + 1)
-                                    + ") irse por...(" + (i + 1) + ", "
-                                    + caminos[i][j] + ", " + (j + 1) + ")\n";
-                        }
-
-                    }
-                }
-            }
-        }
-        return "la matriz de caminos mas cortos entre los vertices es:\n " + cadena
-                + "\n los diferentes caminos mas cortos entre vertices son:\n" + caminitos;
     }
 
-    //llegue hasta el minuto 25:31 del video,para no dejar el metodo anterior incompleto
+    
+
+    return "El mapa del recorrido de la central de transporte es el siguiente: \n" +cadena+
+
+            "\n Los diferentes caminos mas cortos entre los centrales de transmilenio:\n"+caminitos;
+
+    }
+
+    public String caminosR(int i, int k, String[][] caminosAuxiliares, String caminoRecorrido) {
+
+        if(caminosAuxiliares[i][k].equals("")){
+
+            return "";
+
+         }else{
+
+            caminoRecorrido +=caminosR(i, Integer.parseInt(caminosAuxiliares[i][k].toString()), caminosAuxiliares, caminoRecorrido)+(Integer.parseInt(caminosAuxiliares[i][k].toString())+1)+", ";
+
+            return caminoRecorrido;
+
+        }
+
+    }
+
+
+    
+            
+    
 }
--- Taller grafos Autopista/Taller/src/main/java/Grafo/Main.java
+++ Taller grafos Autopista/Taller/src/main/java/Grafo/Main.java
@@ -12,8 +12,18 @@ public class Main {
     static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        log.debug("Hello World : this is a debug message");
-        log.info("Hello World : this is an info message");
+     long matrizA[][]={{0,3,4,999999999,8,999999999}, {999999999,0,999999999,
+
+            999999999,5,999999999}, {999999999, 999999999, 0, 999999999, 3, 999999999},
+
+            {999999999,999999999,999999999,0,999999999,999999999}, {999999999, 999999999,
+
+                999999999, 7, 0, 3}, {999999999, 999999999, 999999999, 2, 999999999, 0}};
+
+        CaminosMInimos ruta= new CaminosMInimos();
+
+         System.out.println(ruta.algoritmoFloid(matrizA));
+
     }
 
 }
